e3409af092278a5ce0f34e58b9e0dc6dc4f73d03
moonblade <moonblade168@gmail.com>
2020-12-06 21:49:45 +0530

    Jumping character

---
+++ 2-mario/src/Actors/Actor.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+class_name Actor
+
+const FLOOR_NORMAL := Vector2.UP
+var velocity:= Vector2.ZERO
+export var gravity:= 3000.0
+export var speed:= Vector2(300.0, 1000.0)
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
