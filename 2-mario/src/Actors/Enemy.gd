35a082bf1fec7c21b11050753e2e8c53111cbe4a
moonblade <moonblade168@gmail.com>
2020-12-06 22:49:38 +0530

    camera

--- 2-mario/src/Actors/Enemy.gd
+++ 2-mario/src/Actors/Enemy.gd
@@ -12,5 +12,6 @@ func _physics_process(delta: float) -> void:
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
 
 func _ready() -> void:
+	set_physics_process(false)
 	_velocity.x = -speed.x
 
