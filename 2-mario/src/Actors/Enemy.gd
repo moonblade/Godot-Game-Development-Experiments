a6e5cf2fc769246c043b6ba946a731b2d3b8182f
moonblade <moonblade168@gmail.com>
2020-12-07 12:22:42 +0530

    can stomp enemies

--- 2-mario/src/Actors/Enemy.gd
+++ 2-mario/src/Actors/Enemy.gd
@@ -1,9 +1,14 @@
 extends "res://src/Actors/Actor.gd"
 
+func _ready() -> void:
+	set_physics_process(false)
+	_velocity.x = -speed.x
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+func _on_StompDetector_body_entered(body: Node) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
 
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
@@ -11,7 +16,4 @@ func _physics_process(delta: float) -> void:
 		_velocity.x *= -1
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
 
-func _ready() -> void:
-	set_physics_process(false)
-	_velocity.x = -speed.x
 
